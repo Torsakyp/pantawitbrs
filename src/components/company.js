7efc38d887593d1bffa3ea2eb5553008032d82c6
Torsakyp <pantawit.brs@gmail.com>
2023-10-26 21:57:39 +0700

    add logo bemo

--- src/components/company.js
+++ src/components/company.js
@@ -21,6 +21,12 @@ const company = () => {
             class="m-auto"
           />
           <img
+            src={require("../assests/images/bemo.jpeg")}
+            alt="bemo-logo"
+            width="100"
+            class="m-auto"
+          />
+          <img
             src={require("../assests/images/wait2-logo.png")}
             alt="wait-and-logo"
             width="100"
